fe31fec704b701a4407cab1e41cccf4ba64927d5
msuharsa <msuharsa@google.com>
2025-06-21 16:59:32 +0700

    Tambah Data Googlesheet

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -1,22 +1,8 @@
-// src/scenes/bar/index.jsx
-import React, { useState, useEffect } from "react";
 import { Box } from "@mui/material";
-import axios from "axios";
-import Header from "../../components/Header";          // ubah path relatif sesuai struktur folder
-import BarChart from "../../components/BarChart";      // sesuaikan path agar tetap dalam src/
+import Header from "../../components/Header";
+import BarChart from "../../components/BarChart";
 
 const Bar = () => {
-  const [filteredData, setFilteredData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("https://api.sheety.co/8841a2b55e10480aa7475b12fd451f5c/dataGerpas/rekapKab")
-      .then((res) => {
-        setFilteredData(res.data.rekapKab);  // ambil data JSON hasil fetch
-      })
-      .catch((err) => console.error("Gagal ambil data rekapKab:", err));
-  }, []);
-
   return (
     <Box m="20px">
       <Header title="Progres Pendataan" subtitle="Sentra Ekonomi" />
