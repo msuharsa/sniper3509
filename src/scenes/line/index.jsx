950b7fbee900e9b9071ec52a97b03295dde3149f
msuharsa <msuharsa@google.com>
2025-06-22 15:39:53 +0700

    Update BarChart

--- src/scenes/line/index.jsx
+++ src/scenes/line/index.jsx
@@ -14,65 +14,73 @@ const LineChart = () => {
   };
 
   return (
-    {/* Kolom 1 */}
-    <Box
-      display="grid"
-      gridTemplateColumns={{ xs: "1fr", sm: "repeat(12, 1fr)" }}
-      gridAutoRows="140px"
-      gap="20px"
-    >
+    <Box m={{ xs: "10px", sm: "20px" }}>
       <Box
-        gridColumn={{ xs: "span 12", sm: "span 6" }}
-        gridRow="span 4"
-        sx={cardStyle}
+        display="grid"
+        gridTemplateColumns={{ xs: "1fr", sm: "repeat(12, 1fr)" }}
+        gridAutoRows="140px"
+        gap="20px"
       >
-      <Box
-        mt="25px"
-        px="30px"
-        display="flex"
-        flexDirection={{ xs: "column", sm: "row" }}
-        justifyContent="space-between"
-        alignItems={{ xs: "flex-start", sm: "center" }}
-      >
-        <Typography variant="h5" fontWeight="600" mb="15px" color={colors.grey[100]}>
-          Rekap Kabupaten/Kota
-        </Typography>
-        <Box height="560px">
-          <BarChartCustom
-            apiUrl="https://api.sheety.co/8841a2b55e10480aa7475b12fd451f5c/dataGerpas/rekapKab"
-            indexField="kodeKab"
-            valueField="persenSentra"
-            legendText="Kode Kab/Kota"
-            highlightValue="3509"
-          />
+        {/* Kolom 1 */}
+        <Box
+          gridColumn={{ xs: "span 12", sm: "span 6" }}
+          gridRow="span 2"
+          sx={cardStyle}
+        >
+          <Box
+            mt="25px"
+            px="30px"
+            display="flex"
+            flexDirection={{ xs: "column", sm: "row" }}
+            justifyContent="space-between"
+            alignItems={{ xs: "flex-start", sm: "center" }}
+          >
+            <Box>
+              <Typography variant="h5" fontWeight="600" mb="15px" color={colors.grey[100]}>
+                Rekap Kabupaten/Kota
+              </Typography>
+            </Box>
+          </Box>
+          <Box height="560px">
+            <BarChartCustom
+              apiUrl="https://api.sheety.co/8841a2b55e10480aa7475b12fd451f5c/dataGerpas/rekapKab"
+              indexField="kodeKab"
+              valueField="persenSentra"
+              legendText="Kode Kab/Kota"
+              highlightValue="3509"
+            />
+          </Box>
         </Box>
-      </Box>
       
-      {/* Kolom 2 */}
-      <Box
-        gridColumn={{ xs: "span 12", sm: "span 6" }}
-        gridRow="span 4"
-        sx={cardStyle}
-      >
-      <Box
-        mt="25px"
-        px="30px"
-        display="flex"
-        flexDirection={{ xs: "column", sm: "row" }}
-        justifyContent="space-between"
-        alignItems={{ xs: "flex-start", sm: "center" }}
-      >
-        <Typography variant="h5" fontWeight="600" mb="15px" color={colors.grey[100]}>
-          Rekap Kecamatan
-        </Typography>
-        <Box height="560px">
-          <BarChartCustom
-            apiUrl="https://api.sheety.co/8841a2b55e10480aa7475b12fd451f5c/dataGerpas/rekapKec"
-            indexField="namaKecamatan"
-            valueField="persenSentra"
-            legendText="Kecamatan"
-            highlightValue="Sumbersari"
-          />
+        {/* Kolom 2 */}
+        <Box
+          gridColumn={{ xs: "span 12", sm: "span 6" }}
+          gridRow="span 2"
+          sx={cardStyle}
+        >
+          <Box
+            mt="25px"
+            px="30px"
+            display="flex"
+            flexDirection={{ xs: "column", sm: "row" }}
+            justifyContent="space-between"
+            alignItems={{ xs: "flex-start", sm: "center" }}
+          >
+            <Box>
+              <Typography variant="h5" fontWeight="600" mb="15px" color={colors.grey[100]}>
+                Rekap Kabupaten/Kota
+              </Typography>
+            </Box>
+          </Box>
+          <Box height="560px">
+            <BarChartCustom
+              apiUrl="https://api.sheety.co/8841a2b55e10480aa7475b12fd451f5c/dataGerpas/rekapKec"
+              indexField="namaKecamatan"
+              valueField="persenSentra"
+              legendText="Kecamatan"
+              highlightValue="Sumbersari"
+            />
+          </Box>
         </Box>
       </Box>
     </Box>
