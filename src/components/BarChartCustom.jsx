dd654ef96622e8171dec3ccbcf7fb7499a39b7ad
msuharsa <msuharsa@google.com>
2025-06-22 19:23:51 +0700

    Update BarChart

--- src/components/BarChartCustom.jsx
+++ src/components/BarChartCustom.jsx
@@ -5,18 +5,18 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const BarChartCustom = ({
-  apiUrl,
-  indexField,
-  valueField,
-  legendText,
-  highlightCode,
-  isDashboard = false,
+    apiUrl,
+    indexField,
+    valueField,
+    legendText,
+    highlightCode,
+    isDashboard = false,
 }) => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const [chartData, setChartData] = useState([]);
+const theme = useTheme();
+const colors = tokens(theme.palette.mode);
+const [chartData, setChartData] = useState([]);
 
-    useEffect(() => {
+useEffect(() => {
     axios
         .get(apiUrl)
         .then((res) => {
@@ -32,80 +32,92 @@ const BarChartCustom = ({
         setChartData(cleaned);
         })
         .catch((err) => console.error("Gagal fetch data:", err));
-    }, [apiUrl, indexField, valueField]);
+    }, [apiUrl, indexField, valueField]
+);
 
-  const barData = chartData.map((item) => {
+const barData = chartData.map((item) => {
     const name = item[indexField];
     const value = parseFloat(item[valueField]) || 0;
     const isHighlight = highlightCode ? name.includes(highlightCode) : false;
 
     return {
-      indexName: name,
-      value,
-      color: isHighlight ? colors.orangeAccent[300] : colors.orangeAccent[500],
-      isHighlight,
+        indexName: name,
+        value,
+        color: isHighlight ? colors.orangeAccent[300] : colors.orangeAccent[500],
+        isHighlight,
     };
-  });
+});
 
-  const CustomLabelLayer = ({ bars }) =>
-    bars.map((bar) => (
-      <text
-        key={bar.key}
-        x={bar.x + bar.width + 6}
-        y={bar.y + bar.height / 2}
-        alignmentBaseline="middle"
-        fontSize={11}
-        fill={bar.color}
-      >
-        {bar.data.value.toFixed(2)}%
-      </text>
-    ));
-
-  return (
-    <ResponsiveBar
-      layout="horizontal"
-      data={barData}
-      keys={["value"]}
-      indexBy="indexName"
-      margin={{ top: 30, right: isDashboard ? 50 : 80, bottom: isDashboard ? 20 : 40, left: isDashboard ? 70 : 100 }}
-      padding={0.3}
-      colors={({ data }) => data.color}
-      borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
-      axisLeft={{
-        legend: legendText,
-        legendPosition: "middle",
-        legendOffset: -50,
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-      }}
-      axisBottom={null}
-      enableLabel={false}
-      enableGridX={false}
-      enableGridY={false}
-      layers={["grid", "axes", "bars", CustomLabelLayer]}
-      theme={{
-        axis: {
-          ticks: {
-            text: {
-              fill: colors.grey[100],
-            },
-          },
-          legend: {
-            text: {
-              fill: colors.grey[100],
-            },
-          },
-        },
-        tooltip: {
-          container: {
-            background: colors.primary[500],
-            color: "#ffffff",
-          },
-        },
-      }}
-    />
-  );
+    const CustomLabelLayer = ({ bars }) =>
+        bars.map((bar) => (
+            <text
+                key={bar.key}
+                x={bar.x + bar.width + 6}
+                y={bar.y + bar.height / 2}
+                alignmentBaseline="middle"
+                fontSize={11}
+                fill={bar.color}
+            >
+                {bar.data.value.toFixed(2)}%
+            </text>
+        ));
+    return (
+        <ResponsiveBar
+            layout={isDashboard ? "vertical" : "horizontal"}
+            data={barData}
+            keys={["value"]}
+            indexBy="indexName"
+            margin={{ top: 30, right: isDashboard ? 50 : 80, bottom: isDashboard ? 20 : 40, left: isDashboard ? 70 : 100 }}
+            padding={0.3}
+            colors={({ data }) => data.color}
+            borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
+            axisLeft={
+            !isDashboard
+            ?   {
+                    legend: legendText,
+                    legendPosition: "middle",
+                    legendOffset: -50,
+                    tickSize: 5,
+                    tickPadding: 5,
+                    tickRotation: 0,
+                }
+            : null
+            }
+            axisBottom={
+                isDashboard
+                ?   {
+                    legend: "Kode Kabupaten/Kota",
+                    legendPosition: "middle",
+                    legendOffset: 40,
+                    }
+                : null
+            }
+            enableLabel={false}
+            enableGridX={false}
+            enableGridY={false}
+            layers={["grid", "axes", "bars", CustomLabelLayer]}
+            theme={{
+                axis: {
+                ticks: {
+                    text: {
+                        fill: colors.grey[100],
+                    },
+                },
+                legend: {
+                    text: {
+                        fill: colors.grey[100],
+                    },
+                },
+                },
+                tooltip: {
+                    container: {
+                        background: colors.primary[500],
+                        color: "#ffffff",
+                    },
+                },
+            }}
+        />
+    );
 };
 
 export default BarChartCustom;
